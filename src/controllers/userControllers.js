31cb362c5939be3a70677691bc7046d2bdc43b9c
Kim Yong Jae <yooh3574@naver.com>
2021-06-16 14:33:27 +0900

    add url parameters

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,6 @@
 export const join = (req,res) => res.send("join");
 export const edit = (req,res) => res.send("Edit User");
 export const remove = (req,res) => res.send("Remove User");
+export const login = (req,res) => res.sent("Log In");
+export const logout = (req,res) => res.sent("Log Out");
+export const see = (req,res) => res.sent("See User");
