aedd7bf1c7766ae6aa4c468d8ab57efcb1f120e5
Kim Yong Jae <yooh3574@naver.com>
2021-07-08 16:27:46 +0900

    add edit video

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {join,login} from "../controllers/userControllers";
-import {trending,search} from "../controllers/videoControllers";
+import { join, login } from "../controllers/userControllers";
+import { home, search } from "../controllers/videoControllers";
 
 const globalRouter = express.Router();
 
 
-globalRouter.get("/",trending);
-globalRouter.get("/join",join);
-globalRouter.get("/login",login);
+globalRouter.get("/", home);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
 
 export default globalRouter;
--- src/server.js
+++ src/server.js
@@ -1,25 +1,23 @@
-import "./db";
 import express from "express";
 import morgan from "morgan"
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
+
 
 const app = express();
-const logger=morgan("dev");
+const logger = morgan("dev");
 
-app.set("view engine","pug");
-app.set("views", process.cwd()+"/src/views");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
-app.use(logger); 
-app.use(express.urlencoded({extended:true}));
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/",globalRouter);
-app.use("/videos",videoRouter);
-app.use("/users",userRouter);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+export default app;
 
-app.listen(4000, handleListening);
