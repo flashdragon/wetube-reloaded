b3c21818f78481e75b2617a61e190d13448c52a3
Kim Yong Jae <yooh3574@naver.com>
2021-07-20 16:34:19 +0900

    add create account

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,36 @@
-export const join = (req,res) => res.send("join");
-export const edit = (req,res) => res.send("Edit User");
-export const remove = (req,res) => res.send("Remove User");
-export const login = (req,res) => res.sent("Log In");
-export const logout = (req,res) => res.sent("Log Out");
-export const see = (req,res) => res.sent("See User");
+import User from "../models/User";
+
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Create Account" });
+export const postJoin = async (req, res) => {
+    console.log(req.body);
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match.",
+        })
+    }
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    if (exists) {
+        return res.render("join", {
+            pageTitle: "Join",
+            errorMessage: "This username/email is already taken."
+        });
+    }
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    })
+    res.redirect("/login");
+
+}
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
+export const login = (req, res) => res.sent("Log In");
+export const logout = (req, res) => res.sent("Log Out");
+export const see = (req, res) => res.sent("See User");
