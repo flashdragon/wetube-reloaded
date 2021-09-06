3278d781745c0a2a0dc8ddcb14d7dda87e44a029
Kim Yong Jae <yooh3574@naver.com>
2021-09-06 16:53:10 +0900

    add eidt profile and protector middleware

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
-import {edit,remove,see,logout} from "../controllers/userControllers";
+import {
+    getEdit,
+    postEdit,
+    see,
+    logout,
+    startGithubLogin,
+    finishGithubLogin,
+} from "../controllers/userControllers";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.get("/logout",logout);
-userRouter.get("/edit",edit);
-userRouter.get("/remove",remove);
-userRouter.get("/:id",see);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+userRouter.get("/:id", see);
 
 
 export default userRouter;
