c680a2cd02de3e5b854c76aab76f6ba2a4548b3d
Kim Yong Jae <yooh3574@naver.com>
2021-09-27 16:09:17 +0900

    add video upload

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,7 +7,7 @@ import {
     postUpload,
     deleteVideo,
 } from "../controllers/videoControllers";
-import { protectorMiddleware, uploadFiles } from "../middlewares";
+import { protectorMiddleware, videoUpload } from "../middlewares";
 const videoRouter = express.Router();
 
 
@@ -24,7 +24,7 @@ videoRouter
     .route("/upload")
     .all(protectorMiddleware)
     .get(getUpload)
-    .post(postUpload);
+    .post(videoUpload.single("video"), postUpload);
 
 
 export default videoRouter;
